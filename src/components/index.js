b8d8546493468618e0f5b0b0aa8cfb686a814b79
Jil02877 <140379056+Jil02877@users.noreply.github.com>
2025-04-18 11:07:18 +0530

    final commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-import About from "./about/About";
-import Contact from "./contact/Contact";
-import Footer from "./footer/Footer";
-import GitHub from "./github/GitHub";
-import Header from "./header/Header";
-import Home from "./home/Home";
-import User from "./user/User";
+import About from "./about/About.jsx";
+import Contact from "./contact/Contact.jsx";
+import Footer from "./footer/Footer.jsx";
+import GitHub from "./github/GitHub.jsx";
+import Header from "./header/Header.jsx";
+import Home from "./home/Home.jsx";
+import User from "./user/User.jsx";
 
 export {About,Contact,Footer,GitHub,Header,Home,User}
